90f27577a4d53bb3978e0e155a174649495b07a5
Mathias Ezequiel Latronico <mathiezelat@gmail.com>
2021-09-17 18:57:31 -0300

    entregable - clase 3

---
+++ entregable-clase-03/test.js
@@ -0,0 +1,46 @@
+const Contenedor = require('./Contenedor.js');
+
+const miContenedor = new Contenedor('productos.txt');
+
+
+const main = async () => {
+
+    try {
+
+        await miContenedor.save({ 
+            title: 'Mundo', 
+            price: 2000, 
+            thumbnail: 'Imagen de Mundo'
+        });
+        
+        await miContenedor.deleteAll();
+
+        await miContenedor.save({ 
+            title: 'Hola Mundo', 
+            price: 2700, 
+            thumbnail: 'Imagen de Hola Mundo'
+        });
+
+        await miContenedor.save({ 
+            title: 'Tierra', 
+            price: 700, 
+            thumbnail: 'Imagen de Tierra'
+        });
+
+        const productoSolicitado = await miContenedor.getById(1);
+
+        console.log( productoSolicitado );
+
+        await miContenedor.deleteById(1);
+
+        const productos = await miContenedor.getAll();
+
+        console.log( productos );
+
+    } catch ( error ) {
+        console.log( 'Ocurrio un error inesperado:', error );
+    }
+
+}
+
+main();
